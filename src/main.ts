f4d70c7aca7102293170d93d73e5371bcda92c63
inhanbyeol <inhanbyeol94@gmail.com>
2023-08-24 01:59:42 +0900

    [추가] 전역 에러처리, 파이프 설정, 포트 변경

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './_common/utils/exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+  app.useGlobalFilters(new HttpExceptionFilter());
+  await app.listen(3005);
 }
 bootstrap();
