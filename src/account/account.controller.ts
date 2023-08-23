e1d25ce37004dd51b48bd3bcaa492ff3eae57b26
inhanbyeol <inhanbyeol94@gmail.com>
2023-08-24 03:00:46 +0900

    [추가] 예금계좌 타입, 예금계좌 생성 API

--- src/account/account.controller.ts
+++ src/account/account.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AccountService } from './account.service';
+import { CreateAccountTypeDto } from '../_common/dtos/createAccountType.dto';
+import { IMessage } from '../_common/interfaces/message.interface';
+import { CreateAccountDto } from '../_common/dtos/createAccount.dto';
+import { IAccountNumber } from '../_common/interfaces/accountNumber.interface';
 
 @Controller('account')
-export class AccountController {}
+export class AccountController {
+  constructor(private accountService: AccountService) {}
+
+  @Post('type')
+  async createAccountType(@Body() newAccountType: CreateAccountTypeDto): Promise<IMessage> {
+    return await this.accountService.createAccountType(newAccountType);
+  }
+
+  @Post()
+  async createAccount(@Body() newAccount: CreateAccountDto): Promise<IAccountNumber> {
+    return await this.accountService.createAccount(newAccount);
+  }
+}
