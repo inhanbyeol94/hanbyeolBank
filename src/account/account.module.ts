5a69353e13e44c5c5793350744a2125af5347833
inhanbyeol <inhanbyeol94@gmail.com>
2023-08-24 03:02:04 +0900

    [추가] TypeORM, ClientService

--- src/account/account.module.ts
+++ src/account/account.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AccountController } from './account.controller';
 import { AccountService } from './account.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Account } from '../_common/entities/account.entity';
+import { AccountType } from '../_common/entities/accountType.entity';
+import { ClientService } from '../client/client.service';
+import { Client } from '../_common/entities/client.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Account, AccountType, Client])],
   controllers: [AccountController],
-  providers: [AccountService]
+  providers: [AccountService, ClientService],
 })
 export class AccountModule {}
